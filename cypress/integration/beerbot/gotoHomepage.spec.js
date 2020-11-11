ff42a11cdef663a33cd1b4d38a0719d90518b798
rchovatiya88 <rchovatiya88@gmail.com>
2020-11-10 23:59:50 -0500

    init

--- cypress/integration/beerbot/gotoHomepage.spec.js
+++ cypress/integration/beerbot/gotoHomepage.spec.js
@@ -13,6 +13,8 @@ context('Beer buying bot ', () => {
     it('Go to home page and try to buy a product', () => {
         cy.get('.custom-logo').click();
         cy.visit(baseURL + '/product/new-release-sticker-pack/');
+
+
         cy.get('#product-46255 > div.summary.entry-summary > form > button').click();
         cy.wait(1200)
         cy.get('#content > div > div.woocommerce > div > a').click()
